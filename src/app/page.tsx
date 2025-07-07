5771934186c60def7ea63b14b9c3876474c40503
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-07-07 21:40:13 +0530

    Header and project section almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,105 @@
+import Game from "@/components/game";
 import Image from "next/image";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import { BiLinkExternal } from "react-icons/bi";
+import { VscGithub } from "react-icons/vsc";
+import { cn } from "@/lib/utils";
+const projects = [
+  {
+    id: 1,
+    title: "Task Manager",
+    description:
+      "A simple yet intuitive task management app that helps you organize your tasks efficiently. Create, edit, and delete tasks with ease.",
+    image: "/task.webp",
+    buttonText: "Try now →",
+    githubLink: "https://github.com/KRSNAGUPTA/task-manager",
+    liveLink: "https://task.krsnadev.tech/",
+  },
+  {
+    id: 2,
+    title: "Travel Tribe",
+    description:
+      "Next time book your hostel with us. Fast and easy booking for hostels and hotels. We are the best in the business.",
+    image: "/travel-tribe.webp",
+    buttonText: "Try now →",
+    githubLink: "https://github.com/KRSNAGUPTA/traveltribe",
+    liveLink: "https://travel-tribe.krsnadev.tech/",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+      <section id="game-section">
+        <Game />
+      </section>
+      <section id="project-section">
+        <div className="relative flex min-h-screen w-full items-center justify-center bg-black px-4">
+          <div
+            className="
+              absolute inset-0
+              [background-size:40px_40px]
+              [background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]
+            "
+          />
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="relative z-20 flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-8 py-8 w-full max-w-7xl">
+            {projects.map((project) => (
+              <CardContainer className="w-full sm:w-[30rem]" key={project.id}>
+                <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
+                  <CardItem translateZ="50" className="text-xl font-bold text-neutral-100">
+                    {project.title}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-200 text-xs font-semibold antialiased max-w-xs mt-2"
+                  >
+                    {project.description}
+                  </CardItem>
+                  <CardItem
+                    translateZ="100"
+                    className="w-full mt-4 relative max-w-full aspect-video rounded-xl overflow-hidden border"
+                  >
+                    <iframe
+                      src={project.liveLink}
+                      className="w-full h-full border-0"
+                      scrolling="no"
+                      sandbox="allow-scripts allow-same-origin"
+                      title={`${project.title} Live Preview`}
+                    ></iframe>
+                  </CardItem>
+
+                  <div className="flex justify-between items-center mt-10">
+                    <CardItem
+                      href={project.githubLink}
+                      translateZ={20}
+                      as="a"
+                      target="__blank"
+                      className="p-1 rounded-full backdrop-blur-md bg-white/10 border border-white/20 text-white hover:bg-white/20 transition"
+                    >
+                      <VscGithub className="h-7 w-7 text-white" />
+                    </CardItem>
+
+                    <CardItem
+                      href={project.liveLink}
+                      target="__blank"
+                      translateZ={20}
+                      as="button"
+                      className="hover:cursor-pointer flex items-center gap-2 px-4 py-2 rounded-full backdrop-blur-md bg-white/10 border border-white/20 text-white text-xs font-semibold hover:bg-white/20 transition"
+                    >
+                      Try now
+                      <BiLinkExternal className="h-4 w-4" />
+                    </CardItem>
+                  </div>
+                </CardBody>
+              </CardContainer>
+            ))}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
+
